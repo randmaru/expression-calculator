325ad691b807233ae516f16d9d632f36f2274fd2
randmaru <git@randma.ru>
2021-08-31 01:45:41 +0300

    feat: added solution

--- src/index.js
+++ src/index.js
@@ -7,6 +7,21 @@ function expressionCalculator(expr) {
 	let openBracket = 0,
 		closeBracket = 0,
 		string = expr.split("");
+
+  for (let i of string)
+   switch (i) {
+    case ")":
+     closeBracket++;
+     break;
+    case "(":
+     openBracket++;
+     break;
+   }
+
+  if (closeBracket == openBracket) {
+   return Function(`'use stringier'; return (${expr})`)();
+  }
+  throw "ExpressionError: Brackets must be paired";
 }
 
 module.exports = {
